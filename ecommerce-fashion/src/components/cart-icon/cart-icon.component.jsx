004b9929cf89c03a1c7b2efbde38c9813fcb92e3
Sathwick Reddy Mora <sathwickreddy2002@gmail.com>
2023-10-24 12:56:02 -0500

    update the cartdown feature for quantity display

--- ecommerce-fashion/src/components/cart-icon/cart-icon.component.jsx
+++ ecommerce-fashion/src/components/cart-icon/cart-icon.component.jsx
@@ -4,13 +4,19 @@ import { CartContext } from "../../contexts/cart.context";
 import "./cart-icon.styles.scss";
 
 function CartIcon() {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartItems } = useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  const quantityArray = cartItems.map((item) => item.quantity);
+  const totalQuantity = quantityArray.reduce(
+    (previous, current) => previous + current,
+    0
+  );
 
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon"></ShoppingIcon>
-      <span className="item-count">0</span>
+      <span className="item-count">{totalQuantity}</span>
     </div>
   );
 }
