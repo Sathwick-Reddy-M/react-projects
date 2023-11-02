90b6d69d2e3a2db340b0d67d9026099ac75ad470
Sathwick Reddy Mora <sathwickreddy2002@gmail.com>
2023-11-02 14:06:54 -0500

    update the app component using typescript

---
+++ name-finder-function-based/src/App.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect, ChangeEvent } from "react";
+import "./App.css";
+import NameCard from "./components/name-card/name-card.component";
+import NameSearch from "./components/name-search/name-search.component";
+import { getData } from './utils/data.utils';
+
+export type Person = {
+  id: number,
+  firstName: string,
+  lastName: string
+}
+
+export type Persons = {
+  users: Person[]
+}
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [persons, setPersons] = useState<Person[]>([]);
+  const [filtered, setFiltered] = useState(persons);
+
+  useEffect(() => {
+      const fecthUsers = async () => {
+        const users = await getData<Persons>('https://dummyjson.com/users');
+        setPersons(users.users);
+      }
+      fecthUsers();
+  }, []);
+
+  useEffect(() => {
+    console.log(persons)
+    setFiltered(
+      persons.filter((person) => {
+        let name =
+          person.firstName.toLocaleLowerCase() +
+          " " +
+          person.lastName.toLocaleLowerCase();
+        return name.includes(searchTerm);
+      })
+    );
+  }, [persons, searchTerm]);
+
+  const searchEvent = (event: ChangeEvent<HTMLInputElement>): void => {
+    const searchTermValue = event.target.value.toLocaleLowerCase();
+    setSearchTerm(searchTermValue);
+  };
+
+  return (
+    <div>
+      <NameSearch eventHandler={searchEvent}></NameSearch>
+      <NameCard filteredPersons={filtered}></NameCard>
+    </div>
+  );
+}
+
+export default App;
