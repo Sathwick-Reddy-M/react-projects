2b01cd22beeab9177300ae2e413911590bdb2e2f
Sathwick Reddy Mora <sathwickreddy2002@gmail.com>
2023-11-25 20:16:56 -0600

    initial flow

---
+++ todo-list/src/components/task-list/task-list.component.jsx
@@ -0,0 +1,10 @@
+import "./task-list.styles.css";
+
+export function TaskList() {
+  return (
+    <ul>
+      <li>List Item 1</li>
+      <li>List Item 2</li>
+    </ul>
+  );
+}
---
+++ todo-list/src/components/task-search/task-search.component.jsx
@@ -0,0 +1,12 @@
+import "./task-search.styles.css";
+
+export function TaskSearch() {
+  return (
+    <input
+      type="search"
+      name="task-search"
+      id="task-search"
+      placeholder="Search Task"
+    />
+  );
+}
